cf85b784639a29f6b1da979bd1ecf3f21b79a892
Kalline Corrêa <kallinecorrea@hotmail.com>
2024-08-16 22:03:47 -0300

    plotando os resultados no gráfico

--- algoritmos.py
+++ algoritmos.py
@@ -1,6 +1,5 @@
 import random
-def gerar_referencias_de_pagina(num_paginas, tamanho_sequencia):
-    return [random.randint(0, num_paginas - 1) for _ in range(tamanho_sequencia)]
+import matplotlib.pyplot as plt
 
 def fifo(referencia_de_paginas, num_molduras):
     molduras = []
@@ -41,6 +40,13 @@ def aging(referencia_de_paginas, num_molduras, bits=8):
             contador[p] = contador[p] >> 1  
     
     return faltas_de_pagina
+
+def ler_referencias(nome_arquivo):
+    with open(nome_arquivo, 'r') as arquivo:
+        conteudo = arquivo.read()
+    referencias = [int(x.strip()) for x in conteudo.split(',')]
+    return referencias
+
 def main():
     lista_num_molduras = list(range(1, 21))
 
@@ -48,10 +54,8 @@ def main():
 
     num_paginas = 200
     tamanho_sequencia = 1000
-    referencia_de_paginas = gerar_referencias_de_pagina(num_paginas, tamanho_sequencia)
 
-    with open("referencias_paginas.txt", "w") as f:
-        f.write('|'.join(map(str, referencia_de_paginas)))
+    referencia_de_paginas = ler_referencias('referencias_paginas.txt')
 
     resultados = []
 
@@ -68,5 +72,23 @@ def main():
     for resultado in resultados:
         print(f"molduras: {resultado['molduras']}, Falta de páginas FIFO: {resultado['falta_de_paginas_fifo']}, Falta de páginas AGING: {resultado['falta_de_paginas_aging']}")
 
+    molduras = [resultado['molduras'] for resultado in resultados]
+    faltas_fifo = [resultado['falta_de_paginas_fifo'] for resultado in resultados]
+    faltas_aging = [resultado['falta_de_paginas_aging'] for resultado in resultados]
+
+    plt.figure(figsize=(12, 6))
+    
+    plt.plot(molduras, faltas_fifo, marker='o', label='FIFO', color='b')
+    plt.plot(molduras, faltas_aging, marker='x', label='AGING', color='r')
+    
+    plt.xlabel('Número de Molduras')
+    plt.ylabel('Número de Faltas de Página')
+    plt.title('Comparação de Faltas de Página entre FIFO e AGING')
+    plt.legend()
+    plt.grid(True)
+    
+    plt.savefig('comparacao_algoritmos.png')
+    plt.show()
+    
 if __name__ == "__main__":
     main()
